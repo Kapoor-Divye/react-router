05859adbe08c02f9c7033b1bdd13b1a35cd1bc21
Kapoor-Divye <divyekapoorgzb@gmail.com>
2026-05-16 20:44:33 +0530

    fixes in footer, header, home and main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom';
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
 import Layout from './Layout.jsx';  
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
